1ec5f565e1f5d2fc31435f9742e4a5b79b558901
Žan Žibert <zzibert@plume.com>
2021-10-13 21:35:19 +0200

    First Principles

--- src/main/scala/part2_primer/FirstPrinciples.scala
+++ src/main/scala/part2_primer/FirstPrinciples.scala
@@ -12,69 +12,78 @@ object FirstPrinciples extends App {
   implicit val materializer = ActorMaterializer()
 
   // sources
-  val source = Source(1 to 10)
-  // sinks
+
+  // sink
   val sink = Sink.foreach[Int](println)
 
-  val graph = source.to(sink)
-  //  graph.run()
+//  val graph = source.to(sink)
+
+//  graph.run()
 
   // flows transform elements
   val flow = Flow[Int].map(x => x + 1)
-  val sourceWithFlow = source.via(flow)
+
+//  val sourceWithFlow = source.via(flow)
+
   val flowWithSink = flow.to(sink)
 
-  //  sourceWithFlow.to(sink).run()
-  //  source.to(flowWithSink).run()
-  //  source.via(flow).to(sink).run()
+//  sourceWithFlow.to(sink).run()
 
-  // nulls are NOT allowed
-  // val illegalSource = Source.single[String](null)
-  // illegalSource.to(Sink.foreach(println)).run()
-  // use Options instead
+//  source.to(flowWithSink).run()
 
-  // various kinds of sources
+//  source.via(flow).to(sink).run()
+
+  // nulls are not allowed
+//  val illegalSource = Source.single[String](null)
+
+//  illegalSource.to(Sink.foreach(println))
+
   val finiteSource = Source.single(1)
+
   val anotherFiniteSource = Source(List(1, 2, 3))
+
   val emptySource = Source.empty[Int]
-  val infiniteSource = Source(Stream.from(1)) // do not confuse an Akka stream with a "collection" Stream
+
+  val infiniteSource = Source(Stream.from(1)) // do not confuse akka stream with a collection stream
+
   import scala.concurrent.ExecutionContext.Implicits.global
+
   val futureSource = Source.fromFuture(Future(42))
 
   // sinks
   val theMostBoringSink = Sink.ignore
-  val foreachSink = Sink.foreach[String](println)
+
+  val forEachSink = Sink.foreach[String](println)
+
   val headSink = Sink.head[Int] // retrieves head and then closes the stream
-  val foldSink = Sink.fold[Int, Int](0)((a, b) => a + b)
+
+  val foldSink = Sink.fold[Int, Int](0)(_ + _)
 
   // flows - usually mapped to collection operators
-  val mapFlow = Flow[Int].map(x => 2 * x)
-  val takeFlow = Flow[Int].take(5)
-  // drop, filter
-  // NOT have flatMap
+  val mapFlow = Flow[Int].map(_ * 2)
 
-  // source -> flow -> flow -> ... -> sink
-  val doubleFlowGraph = source.via(mapFlow).via(takeFlow).to(sink)
-  //  doubleFlowGraph.run()
+  val takeFlow = Flow[Int].take(5)
+
+  // not have flatMap
+
+  // source -> flow -> flow -> sink
+
+  val source = Source(1 to 20)
+
+  val doubleFlowGraph = source.via(mapFlow).via(takeFlow).via(mapFlow).to(sink)
+
+//  doubleFlowGraph.run()
 
   // syntactic sugars
-  val mapSource = Source(1 to 10).map(x => x * 2) // Source(1 to 10).via(Flow[Int].map(x => x * 2))
+  val mapSource = Source(1 to 10).map(_ * 2) // Source(1 to 10).via(Flow[Int](_ * 2))
+
   // run streams directly
-  //  mapSource.runForeach(println) // mapSource.to(Sink.foreach[Int](println)).run()
+//  mapSource.runForeach(println) // mapSource.to(Sink.foreach[Int](println)).run()
 
-  // OPERATORS = components
+  // operators = components
 
-  /**
-    * Exercise: create a stream that takes the names of persons, then you will keep the first 2 names with length > 5 characters.
-    *
-    */
-  val names = List("Alice", "Bob", "Charlie", "David", "Martin", "AkkaStreams")
-  val nameSource = Source(names)
-  val longNameFlow = Flow[String].filter(name => name.length > 5)
-  val limitFlow = Flow[String].take(2)
-  val nameSink = Sink.foreach[String](println)
+  // exercise
+  // create a stream that takes the names of persons, first two names, with length > 5 characters
 
-  nameSource.via(longNameFlow).via(limitFlow).to(nameSink).run()
-  nameSource.filter(_.length > 5).take(2).runForeach(println)
-
+  Source(List("joze", "polde", "franci", "benjamin", "gozdni joza", "roxy")).filter(_.length > 5).take(2).runForeach(println)
 }
