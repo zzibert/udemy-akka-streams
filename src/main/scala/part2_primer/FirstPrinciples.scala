e1fa9c635ab6e0e336ddb3f46d22952ea82047fb
Žan Žibert <zzibert@plume.com>
2021-10-30 10:04:26 +0200

    First Principles

--- src/main/scala/part2_primer/FirstPrinciples.scala
+++ src/main/scala/part2_primer/FirstPrinciples.scala
@@ -3,87 +3,86 @@ package part2_primer
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Sink, Source}
-
 import scala.concurrent.Future
 
 object FirstPrinciples extends App {
-
   implicit val system = ActorSystem("FirstPrinciples")
+
   implicit val materializer = ActorMaterializer()
 
-  // sources
+  // Source
+  val source = Source(1 to 10)
 
-  // sink
+  // Sink
   val sink = Sink.foreach[Int](println)
 
-//  val graph = source.to(sink)
+  val graph = source.to(sink)
 
 //  graph.run()
 
-  // flows transform elements
-  val flow = Flow[Int].map(x => x + 1)
+  // flows transforms elements
+  val flow = Flow[Int].map(_ + 1)
 
-//  val sourceWithFlow = source.via(flow)
+  val sourceWithFlow = source.via(flow)
 
   val flowWithSink = flow.to(sink)
 
 //  sourceWithFlow.to(sink).run()
 
-//  source.to(flowWithSink).run()
+//  source.via(flow).via(flow).to(sink).run()
 
-//  source.via(flow).to(sink).run()
-
-  // nulls are not allowed
+  // nulls are NOT allowed
+  // use options instead
 //  val illegalSource = Source.single[String](null)
 
-//  illegalSource.to(Sink.foreach(println))
+//  illegalSource.to(Sink.foreach(println)).run()
 
   val finiteSource = Source.single(1)
-
   val anotherFiniteSource = Source(List(1, 2, 3))
 
   val emptySource = Source.empty[Int]
 
-  val infiniteSource = Source(Stream.from(1)) // do not confuse akka stream with a collection stream
+  // do not confuse akka streams with collection stream
+  val infiniteSource = Source(Stream.from(1))
 
   import scala.concurrent.ExecutionContext.Implicits.global
 
   val futureSource = Source.fromFuture(Future(42))
 
-  // sinks
+  // sink
   val theMostBoringSink = Sink.ignore
 
   val forEachSink = Sink.foreach[String](println)
 
-  val headSink = Sink.head[Int] // retrieves head and then closes the stream
+  val headSink = Sink.head[Int] // retries head and then closes the stream
 
   val foldSink = Sink.fold[Int, Int](0)(_ + _)
 
   // flows - usually mapped to collection operators
   val mapFlow = Flow[Int].map(_ * 2)
-
   val takeFlow = Flow[Int].take(5)
 
-  // not have flatMap
+  // NOT have flatMap
 
-  // source -> flow -> flow -> sink
+  // source -> flow -> flow -> ... -> sink
 
-  val source = Source(1 to 20)
+//  val doubleFlowGraph = source.via(mapFlow).via(takeFlow).to(sink).run()
 
-  val doubleFlowGraph = source.via(mapFlow).via(takeFlow).via(mapFlow).to(sink)
-
-//  doubleFlowGraph.run()
-
-  // syntactic sugars
-  val mapSource = Source(1 to 10).map(_ * 2) // Source(1 to 10).via(Flow[Int](_ * 2))
+  val mapSource = Source(1 to 10).map(_ * 2)
 
   // run streams directly
-//  mapSource.runForeach(println) // mapSource.to(Sink.foreach[Int](println)).run()
+  //  mapSource.runForeach(println) // mapSource.to(Sink.foreach[Int](println))
 
-  // operators = components
+  // OPERATORS = components
 
-  // exercise
-  // create a stream that takes the names of persons, first two names, with length > 5 characters
+  /*
+  * create a stream that takes names of persons,
+  * then keep the first two with length greater than 5 characters
+  * */
 
-  Source(List("joze", "polde", "franci", "benjamin", "gozdni joza", "roxy")).filter(_.length > 5).take(2).runForeach(println)
+  val names = List("james", "giannis", "mike", "bob", "alfred", "abraham", "william")
+
+  Source(names).filter(_.length > 5).take(2).runForeach(println)
+
+
 }
