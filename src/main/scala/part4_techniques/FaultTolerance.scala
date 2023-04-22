92e178f0c62f55772f134794b55bfd2998a669db
Žan Žibert <zzibert@plume.com>
2023-04-22 13:09:00 +0200

    Using the MergeHub

--- src/main/scala/part4_techniques/FaultTolerance.scala
+++ src/main/scala/part4_techniques/FaultTolerance.scala
@@ -2,10 +2,13 @@ package part4_techniques
 
 import akka.{Done, NotUsed}
 import akka.actor.{Actor, ActorSystem, Cancellable}
-import akka.stream.javadsl.ZipWith
-import akka.stream.{ActorMaterializer, ClosedShape, CompletionStrategy, FanInShape, FlowShape, Graph, Inlet, Materializer, Outlet, OverflowStrategy, Shape, SourceShape, UniformFanInShape}
-import akka.stream.scaladsl.{Balance, Broadcast, Flow, GraphDSL, Keep, Merge, MergePreferred, RunnableGraph, Sink, Source, Zip}
+import akka.stream.scaladsl.GraphDSL.Implicits.{FanInOps, SourceArrow, fanOut2flow}
+import akka.stream.scaladsl.Tcp.OutgoingConnection
+import akka.stream.scaladsl.{Balance, Broadcast, Concat, Flow, GraphDSL, Keep, Merge, MergeHub, MergePreferred, RunnableGraph, Sink, Source, Tcp, Zip, ZipWith}
+import akka.stream.{ActorMaterializer, Attributes, ClosedShape, CompletionStrategy, DelayOverflowStrategy, FanInShape, FlowShape, Graph, Inlet, KillSwitches, Materializer, Outlet, OverflowStrategy, Shape, SourceShape, UniformFanInShape}
+import akka.util.ByteString
 import org.scalatest.Matchers.{convertToAnyShouldWrapper, equal}
+import scala.annotation.unchecked.uncheckedVariance
 import scala.collection.immutable
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.{Await, ExecutionContext, Future, Promise}
@@ -18,67 +21,23 @@ object FaultTolerance extends App {
 
   implicit val ec: ExecutionContext = ExecutionContext.global
 
-  import FanInShape.{Init, Name}
 
-  //  class PriorityWorkerPoolShape2[In, Out](_init: Init[Out] = Name("PriorityWorkerPool"))
-  //    extends FanInShape[Out](_init) {
-  //    protected override def construct(i: Init[Out]) = new PriorityWorkerPoolShape2(i)
-  //
-  //    val jobsIn = newInlet[In]("jobsIn")
-  //    val priorityJobsIn = newInlet[In]("priorityJobsIn")
-  //    // Outlet[Out] with name "out" is automatically created
-  //  }
+  // A simple consumer that will print to the console for now
+  val consumer = Sink.foreach(println)
 
-  // A shape represents the input and output ports of a reusable
-  // processing module
-  case class PriorityWorkerPoolShape[In, Out](jobsIn: Inlet[In], priorityJobsIn: Inlet[In], resultsOut: Outlet[Out])
-    extends Shape {
+  // Attach a MergeHub Source to the consumer. This will materialize to a
+  // corresponding Sink.
+  val runnableGraph: RunnableGraph[Sink[String, NotUsed]] =
+  MergeHub.source[String](perProducerBufferSize = 16).to(consumer)
 
-    // It is important to provide the list of all input and output
-    // ports with a stable order. Duplicates are not allowed.
-    override val inlets: immutable.Seq[Inlet[_]] =
-    jobsIn :: priorityJobsIn :: Nil
-    override val outlets: immutable.Seq[Outlet[_]] =
-      resultsOut :: Nil
+  // By running/materializing the consumer we get back a Sink, and hence
+  // now have access to feed elements into it. This Sink can be materialized
+  // any number of times, and every element that enters the Sink will
+  // be consumed by our consumer.
+  val toConsumer: Sink[String, NotUsed] = runnableGraph.run()
 
-    // A Shape must be able to create a copy of itself. Basically
-    // it means a new instance with copies of the ports
-    override def deepCopy() =
-      PriorityWorkerPoolShape(jobsIn.carbonCopy(), priorityJobsIn.carbonCopy(), resultsOut.carbonCopy())
-
-  }
-
-  object PriorityWorkerPool {
-    def apply[In, Out](
-                        worker: Flow[In, Out, Any],
-                        workerCount: Int): Graph[PriorityWorkerPoolShape[In, Out], NotUsed] = {
-
-      GraphDSL.create() { implicit b =>
-        import GraphDSL.Implicits._
-
-        val priorityMerge = b.add(MergePreferred[In](1))
-        val balance = b.add(Balance[In](workerCount))
-        val resultsMerge = b.add(Merge[Out](workerCount))
-
-        // After merging priority and ordinary jobs, we feed them to the balancer
-        priorityMerge ~> balance
-
-        // Wire up each of the outputs of the balancer to a worker flow
-        // then merge them back
-        for (i <- 0 until workerCount)
-          balance.out(i) ~> worker ~> resultsMerge.in(i)
-
-        // We now expose the input ports of the priorityMerge and the output
-        // of the resultsMerge as our PriorityWorkerPool ports
-        // -- all neatly wrapped in our domain specific Shape
-        PriorityWorkerPoolShape(
-          jobsIn = priorityMerge.in(0),
-          priorityJobsIn = priorityMerge.preferred,
-          resultsOut = resultsMerge.out)
-      }
-
-    }
-
-  }
+  // Feeding two independent sources into the hub.
+  Source.tick(0.millis, 500.millis, "ihihihih").runWith(toConsumer)
+  Source.tick(0.millis, 300.millis, "Trololo").runWith(toConsumer)
 
 }
