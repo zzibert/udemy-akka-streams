1f6b1710abed813026c58ee6bc5f2fefe7c6893d
Žan Žibert <zzibert@plume.com>
2023-04-23 09:21:22 +0200

    Create custom graph stage logic

--- src/main/scala/part4_techniques/FaultTolerance.scala
+++ src/main/scala/part4_techniques/FaultTolerance.scala
@@ -22,17 +22,33 @@ object FaultTolerance extends App {
 
   implicit val ec: ExecutionContext = ExecutionContext.global
 
+  import akka.stream.Attributes
+  import akka.stream.Outlet
   import akka.stream.SourceShape
   import akka.stream.stage.GraphStage
+  import akka.stream.stage.GraphStageLogic
+  import akka.stream.stage.OutHandler
 
   class NumbersSource extends GraphStage[SourceShape[Int]] {
-    // Define the (sole) output port of this stage
     val out: Outlet[Int] = Outlet("NumbersSource")
-    // Define the shape of this stage, which is SourceShape with the port we defined above
     override val shape: SourceShape[Int] = SourceShape(out)
 
-    // This is where the actual (possibly stateful) logic will live
-    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = ???
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+      new GraphStageLogic(shape) {
+        // All state MUST be inside the GraphStageLogic,
+        // never inside the enclosing GraphStage.
+        // This state is safe to access and modify from all the
+        // callbacks that are provided by GraphStageLogic and the
+        // registered handlers.
+        private var counter = 1
+
+        setHandler(out, new OutHandler {
+          override def onPull(): Unit = {
+            push(out, counter)
+            counter += 1
+          }
+        })
+      }
   }
 
 }
