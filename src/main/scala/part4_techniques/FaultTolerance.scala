a620ec978159cf8497eadfba157051c3c82b5427
Žan Žibert <zzibert@plume.com>
2023-04-29 20:26:47 +0200

    Custom Filter graphStage

--- src/main/scala/part4_techniques/FaultTolerance.scala
+++ src/main/scala/part4_techniques/FaultTolerance.scala
@@ -5,7 +5,7 @@ import akka.actor.{Actor, ActorSystem, Cancellable}
 import akka.stream.scaladsl.GraphDSL.Implicits.{FanInOps, SourceArrow, fanOut2flow}
 import akka.stream.scaladsl.Tcp.OutgoingConnection
 import akka.stream.scaladsl.{Balance, Broadcast, BroadcastHub, Concat, Flow, GraphDSL, Keep, Merge, MergeHub, MergePreferred, PartitionHub, RunnableGraph, Sink, Source, Tcp, Zip, ZipWith}
-import akka.stream.stage.GraphStageLogic
+import akka.stream.stage.{GraphStageLogic, OutHandler}
 import akka.stream.{ActorMaterializer, Attributes, ClosedShape, CompletionStrategy, DelayOverflowStrategy, FanInShape, FlowShape, Graph, Inlet, KillSwitches, Materializer, Outlet, OverflowStrategy, Shape, SourceShape, UniformFanInShape, UniqueKillSwitch}
 import akka.util.ByteString
 import org.scalatest.Matchers.{convertToAnyShouldWrapper, equal}
@@ -23,32 +23,30 @@ object FaultTolerance extends App {
   implicit val ec: ExecutionContext = ExecutionContext.global
 
   import akka.stream.Attributes
-  import akka.stream.Outlet
-  import akka.stream.SourceShape
-  import akka.stream.stage.GraphStage
-  import akka.stream.stage.GraphStageLogic
-  import akka.stream.stage.OutHandler
-
-  import akka.stream.Attributes
   import akka.stream.Inlet
   import akka.stream.SinkShape
   import akka.stream.stage.GraphStage
   import akka.stream.stage.GraphStageLogic
   import akka.stream.stage.InHandler
 
-  class StdoutSink extends GraphStage[SinkShape[Int]] {
-    val in: Inlet[Int] = Inlet("StdoutSink")
-    override val shape: SinkShape[Int] = SinkShape(in)
+  class Filter[A](p: A => Boolean) extends GraphStage[FlowShape[A, A]] {
+
+    val in = Inlet[A]("Filter.in")
+    val out = Outlet[A]("Filter.out")
+
+    val shape = FlowShape.of(in, out)
 
     override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
       new GraphStageLogic(shape) {
-
-        // This requests one element at the Sink startup.
-        override def preStart(): Unit = pull(in)
-
         setHandler(in, new InHandler {
           override def onPush(): Unit = {
-            println(grab(in))
+            val elem = grab(in)
+            if (p(elem)) push(out, elem)
+            else pull(in)
+          }
+        })
+        setHandler(out, new OutHandler {
+          override def onPull(): Unit = {
             pull(in)
           }
         })
