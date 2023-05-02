5478a4a580dc9e15cfe8226bdceaa9f4b995f26d
Žan Žibert <zzibert@plume.com>
2023-05-02 12:17:18 +0200

    How  MapAsync works

--- src/main/scala/part4_techniques/FaultTolerance.scala
+++ src/main/scala/part4_techniques/FaultTolerance.scala
@@ -1,20 +1,11 @@
 package part4_techniques
 
-import akka.{Done, NotUsed}
+
 import akka.actor.{Actor, ActorSystem, Cancellable}
-import akka.stream.scaladsl.GraphDSL.Implicits.{FanInOps, SourceArrow, fanOut2flow}
-import akka.stream.scaladsl.Tcp.OutgoingConnection
 import akka.stream.scaladsl.{Balance, Broadcast, BroadcastHub, Concat, Flow, GraphDSL, Keep, Merge, MergeHub, MergePreferred, PartitionHub, RunnableGraph, Sink, Source, Tcp, Zip, ZipWith}
-import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler, TimerGraphStageLogic}
 import akka.stream.{ActorMaterializer, Attributes, ClosedShape, CompletionStrategy, DelayOverflowStrategy, FanInShape, FlowShape, Graph, Inlet, KillSwitches, Materializer, Outlet, OverflowStrategy, Shape, SourceShape, UniformFanInShape, UniqueKillSwitch}
-import akka.util.ByteString
-import java.util.concurrent.ThreadLocalRandom
-import org.scalatest.Matchers.{convertToAnyShouldWrapper, equal}
-import scala.annotation.unchecked.uncheckedVariance
-import scala.collection.immutable
-import scala.concurrent.duration.{DurationInt, FiniteDuration}
+import java.util.concurrent.atomic.AtomicInteger
 import scala.concurrent.{Await, ExecutionContext, Future, Promise}
-import scala.util.{Failure, Success}
 
 object FaultTolerance extends App {
   val system = ActorSystem()
@@ -23,52 +14,35 @@ object FaultTolerance extends App {
 
   implicit val ec: ExecutionContext = ExecutionContext.global
 
-  // each time an event is pushed through it will trigger a period of silence
-  class TimedGate[A](silencePeriod: FiniteDuration) extends GraphStage[FlowShape[A, A]] {
+  class SometimesSlowService(implicit ec: ExecutionContext) {
 
-    val in = Inlet[A]("TimedGate.in")
-    val out = Outlet[A]("TimedGate.out")
+    private val runningCount = new AtomicInteger
 
-    val shape = FlowShape.of(in, out)
-
-    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
-      new TimerGraphStageLogic(shape) {
-
-        var open = false
-
-        setHandler(in, new InHandler {
-          override def onPush(): Unit = {
-            val elem = grab(in)
-            if (open) pull(in)
-            else {
-              push(out, elem)
-              open = true
-              scheduleOnce(None, silencePeriod)
-            }
-          }
-        })
-        setHandler(out, new OutHandler {
-          override def onPull(): Unit = {
-            pull(in)
-          }
-        })
-
-        override protected def onTimer(timerKey: Any): Unit = {
-          open = false
-        }
+    def convert(s: String): Future[String] = {
+      println(s"running: $s (${runningCount.incrementAndGet()})")
+      Future {
+        if (s.nonEmpty && s.head.isLower)
+          Thread.sleep(500)
+        else
+          Thread.sleep(20)
+        println(s"completed: $s (${runningCount.decrementAndGet()})")
+        s.toUpperCase
       }
+    }
   }
 
-  val sourceTick = Source.tick(50.millis, 50.millis, "Tick")
 
-  val sourceNumber = Source.fromIterator(() => Iterator.from(1))
+  val service = new SometimesSlowService
 
-  val sourceCombined = sourceTick.zipWith(sourceNumber) {(tick, number) =>
-    s"$tick : $number"
-  }
+  Source(List("a", "B", "C", "D", "e", "F", "g", "H", "i", "J"))
+    .map(elem => {
+      println(s"before: $elem"); elem
+    })
+    .mapAsync(4)(service.convert)
+    .to(Sink.foreach(elem => println(s"after: $elem")))
+    .withAttributes(Attributes.inputBuffer(initial = 4, max = 4))
+    .run()
 
-  val timerFlow = Flow.fromGraph(new TimedGate[String](200.millis))
-
-  sourceCombined.via(timerFlow).runForeach(println) // 1, 5, 9, 13
+  // A, B, C, D, E, F, G, ...
 
 }
